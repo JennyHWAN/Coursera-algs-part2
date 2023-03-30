5be95353d6e645c8f20d0efcdad56a8313474468
JennyHWAN <jenny_h310@126.com>
2023-03-31 00:30:45 +0800

    initial file-v2

--- BoggleSolver.java
+++ BoggleSolver.java
@@ -1,12 +1,24 @@
 public class BoggleSolver {
+    String[] dic;
+
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
-    public BoggleSolver(String[] dictionary)
+    public BoggleSolver(String[] dictionary) {
+        dic = dictionary;
+    }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board)
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
-    public int scoreOf(String word)
+    public int scoreOf(String word) {
+        int score = 0;
+        for (int i = 0; i < dic.length; i++) {
+            if (word.equals(dic[i])) {
+                score += 1;
+            }
+        }
+        return score;
+    }
 }
